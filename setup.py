126a0bc97bfbe466ca34a7b22934fa6a4ce13f6a
Barron Hagerman <barron.hagerman@banno.com>
2016-09-27 16:25:19 -0500

    Add Barron Hagerman as author

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 setup(
     name='sentry-ldap-auth',
     version='2.2',
-    author='Chad Killingsworth - Jack Henry and Associates, Inc.',
+    author='Chad Killingsworth <chad.killingsworth@banno.com>, Barron Hagerman <barron.hagerman@banno.com>',
     author_email='chad.killingsworth@banno.com',
     url='http://github.com/banno/getsentry-ldap-auth',
     description='A Sentry extension to add an LDAP server as an authention source.',
