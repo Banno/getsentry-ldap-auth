161b106114730cdfaac75117e9bc0d85cbf12fd4
Barron Hagerman <barron.hagerman@banno.com>
2016-10-05 15:32:29 -0500

    fix "duplicate key" error

--- sentry_ldap_auth/backend.py
+++ sentry_ldap_auth/backend.py
@@ -24,10 +24,12 @@ class SentryLdapBackend(LDAPBackend):
         except ImportError:
             pass
         else:
-            UserEmail.objects.update(
-                user=user,
-                email=ldap_user.attrs.get('mail', ' ')[0] or '',
-            )
+            userEmail = UserEmail.objects.get(user=user)
+            if not userEmail:
+                userEmail = UserEmail.objects.create(user=user)
+
+            userEmail.email=ldap_user.attrs.get('mail', ' ')[0] or ''
+            userEmail.save()
 
         # Check to see if we need to add the user to an organization
         if not settings.AUTH_LDAP_DEFAULT_SENTRY_ORGANIZATION:
